0c52d4eae14d2e345fc25fd3c665f54e6a57fd63
GabrielPeu <gabrielpeu9@gmail.com>
2025-11-04 09:48:31 -0400

    codigo
    Inicio do codigo

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import matplotlib.pyplot as plt
-import pandas as pan
-import numpy 
-import networkx as netx
+import pandas as pd
+import numpy as np
+import networkx as ntx
+import sklearn.feature_extraction.text as skt
+import sklearn.cluster as skc
+
+ 
+df = pd.read_csv('disney_plus_title_list.csv')
+corpus = df['description'].tolist()
+vectorizer = skt.TfidfVectorizer()
+X = vectorizer.fit_transform(corpus)
+
+#print(X.shape[1])
+
+kmeans = skc.MiniBatchKMeans(n_clusters=100).fit(X)
+#print(kmeans.cluster_centers_)
+
+
