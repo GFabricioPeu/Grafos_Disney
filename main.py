56de3199105fb417f77a9985c9dcbee36b777f03
GabrielPeu <gabrielpeu9@gmail.com>
2025-11-05 17:38:35 -0400

    codigo quase pronto

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import networkx as nx
 import sklearn.feature_extraction.text as skt
 import sklearn.cluster as skc
+from sklearn.metrics.pairwise import cosine_similarity
 import re
 
 remover = {'as','the', 'and', 'if','that','in','when','so','where','at','but','or','to'}
@@ -43,21 +44,32 @@ def trata_arquivo (corpus):
 
 def cria_grafo (arquivo):
     df = pd.read_csv(arquivo)
-    corpus = df[['show_id','type','title','director','cast','country','rating','listed_in','description']].fillna('').tolist()
+    corpus = df[['show_id','type','title','director','cast','country','rating','listed_in','description']].fillna('')
     
-    for collums in ['type','title','director','cast','country','listed_in','description']:
+   
+    vectorizer = skt.TfidfVectorizer(stop_words=remover)
+    matriz_vetorizada = vectorizer.fit_transform(corpus['description'])
 
-        vectorizer = skt.TfidfVectorizer(stop_words=remover)
-        matriz_vetorizada = vectorizer.fit_transform(corpus)
-
-
-        
     kmeans = skc.MiniBatchKMeans(n_clusters=100,random_state=9).fit(matriz_vetorizada)
+    df['cluster'] = kmeans.labels_
 
     grafo = nx.Graph()
 
+    df_index = df.set_index('show_id')
+    for show_id, row in df_index.iterrows():
+        grafo.add_node(
+            show_id, 
+            title=row.get('title', 'N/A'), 
+            type=row.get('type', 'N/A'),
+            cluster=row.get('cluster', -1)
+        )
 
-
+    similaridade = cosine_similarity(matriz_vetorizada)
+    lim_similaridade = 0.1
+    for i in range(similaridade.shape[0]):
+        for j in range(i+1,similaridade.shape[1]):
+            if similaridade[i,j] > lim_similaridade:
+                grafo.add_edge()
 
 def main():
     arquivo = 'disney_plus_title_list.csv'
